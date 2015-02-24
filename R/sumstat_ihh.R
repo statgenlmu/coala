e53af15c0bcea5d7e03dccbb22bd146bb0f58460
Paul R Staab <staab@bio.lmu.de>
2015-02-24 14:37:05 +0100

    fix iHH statistic

--- R/sumstat_ihh.R
+++ R/sumstat_ihh.R
@@ -1,16 +1,25 @@
 #' @importFrom R6 R6Class
 #' @importFrom rehh data2haplohh calc_ehh
 SumStat_iHH <- R6Class('SumStat_iHH', inherit = SumStat,
-  private = list(position = NA),
+  private = list(position = NA, population = NULL),
   public = list(
+    initialize = function(name, population, group=0) {
+      assert_that(is.numeric(population))
+      assert_that(length(population) == 1)
+      private$population <- population
+      super$initialize(name, group)
+    },
     calculate = function(seg_sites, files, model) {
       assert_that(is.list(seg_sites))
       assert_that(is.model(model))
       pos <- get_snp_positions(seg_sites, model, relative = FALSE)
+      ind <- get_population_indiviuals(model, private$population)
       lapply(1:length(seg_sites), function(locus) {
         assert_that(is.matrix(seg_sites[[locus]]))
-        ehh <- sapply(1:nrow(seg_sites[[locus]]), function(snp) {
-          calc_ehh(self$segsites_to_rehh_data(seg_sites[[locus]], pos[[locus]]),
+        ehh <- sapply(1:ncol(seg_sites[[locus]]), function(snp) {
+          calc_ehh(self$segsites_to_rehh_data(seg_sites[[locus]],
+                                              pos[[locus]],
+                                              ind),
                    mrk = snp, plotehh = FALSE)$ihh
         })
         colnames(ehh) <- pos[[locus]]
@@ -23,14 +32,14 @@ SumStat_iHH <- R6Class('SumStat_iHH', inherit = SumStat,
       write.table(map, file, row.names = FALSE, col.names = FALSE)
       file
     },
-    segsites_to_haplo = function(seg_sites) {
+    segsites_to_haplo = function(seg_sites, ind) {
       file <- tempfile('haplotypes')
-      write.table(cbind(1:ncol(seg_sites), t(seg_sites)), file,
+      write.table(cbind(ind, seg_sites[ind, ]), file,
                   row.names = FALSE, col.names = FALSE)
       file
     },
-    segsites_to_rehh_data = function(seg_sites, pos) {
-      haplo <- self$segsites_to_haplo(seg_sites)
+    segsites_to_rehh_data = function(seg_sites, pos, ind) {
+      haplo <- self$segsites_to_haplo(seg_sites, ind)
       snp_map <- self$segsites_to_snp_map(seg_sites, pos)
       capture.output(rehh <- data2haplohh(haplo, snp_map, recode.allele = TRUE))
       unlink(c(snp_map, haplo))
@@ -53,6 +62,6 @@ SumStat_iHH <- R6Class('SumStat_iHH', inherit = SumStat,
 #'
 #' @inheritParams sumstat_file
 #' @export
-sumstat_iHH <- function(name = 'iHH', group = 0) {
-  SumStat_iHH$new(name, group = group)
+sumstat_iHH <- function(name = 'iHH', population, group = 0) {
+  SumStat_iHH$new(name, population, group = group)
 }
--- tests/testthat/test-sumstat-iHH.R
+++ tests/testthat/test-sumstat-iHH.R
@@ -1,37 +1,45 @@
 context("SumStat iHH")
 
-seg_sites <- matrix(c(1, 0, 0, 0,
-                      1, 1, 0, 1,
-                      1, 0, 0, 1,
-                      1, 0, 0, 1), 4, 4, byrow=TRUE)
-attr(seg_sites, 'positions') = c(0.1, 0.2, 0.5, 0.7)
+seg_sites <- matrix(c(1, 0, 0, 0, 1,
+                      1, 1, 0, 1, 0,
+                      1, 0, 0, 1, 1,
+                      1, 0, 0, 1, 0), 4, 5, byrow=TRUE)
+attr(seg_sites, 'positions') = c(0.1, 0.2, 0.5, 0.7, 0.9)
 model <- CoalModel(4, 1, 337)
 pos <- get_snp_positions(list(seg_sites), model, relative = FALSE)[[1]]
 
 
 test_that("generation of SNP maps works", {
-  stat_iHH <- sumstat_iHH()
+  stat_iHH <- sumstat_iHH(population = 1)
   snp_map <- stat_iHH$segsites_to_snp_map(seg_sites, pos)
   map <- read.table(snp_map, row.names = 1)
-  expect_equal(ncol(map), 4)
+  expect_equal(nrow(map), 5)
   unlink(snp_map)
 })
 
 
 test_that("generation of haplotye file works", {
-  stat_iHH <- sumstat_iHH()
-  haplotypes <- stat_iHH$segsites_to_haplo(seg_sites)
+  stat_iHH <- sumstat_iHH(population = 1)
+  haplotypes <- stat_iHH$segsites_to_haplo(seg_sites, 1:4)
   haplo <- read.table(haplotypes, row.names = 1)
-  expect_equivalent(haplo, as.data.frame(t(seg_sites)))
+  expect_equivalent(haplo, as.data.frame(seg_sites))
   unlink(haplotypes)
 })
 
 
 test_that('calculation of iHH works', {
-  stat_iHH <- sumstat_iHH()
+  stat_iHH <- sumstat_iHH(population = 1)
   iHH <- stat_iHH$calculate(list(seg_sites), NULL, model)
   expect_that(iHH, is_a('list'))
   expect_equal(length(iHH), 1)
   expect_that(iHH[[1]], is_a('matrix'))
-  expect_equal(dim(iHH[[1]]), c(2,4))
+  expect_equal(dim(iHH[[1]]), c(2,5))
+})
+
+
+test_that('iHH works with trios', {
+  model <- model_trios()
+  stats <- simulate(model, pars=c(1,5))
+  ihh <- sumstat_iHH(population = 2)
+  ihh$calculate(stats$seg_sites, NULL, model)
 })
