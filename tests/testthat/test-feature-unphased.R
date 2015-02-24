53225838867ddca20c9a732a38c9838d13bc40ac
Paul R Staab <staab@bio.lmu.de>
2015-02-24 11:06:01 +0100

    fix #29

--- tests/testthat/test-feature-unphased.R
+++ tests/testthat/test-feature-unphased.R
@@ -7,12 +7,18 @@ test_that("unphasing works", {
                                1, 1, 0, 1,
                                1, 0, 0, 1,
                                1, 0, 0, 1), 4, 4, byrow=TRUE)
+  attr(seg_sites[[1]], 'positions') = c(0.1, 0.2, 0.5, 0.7)
+  attr(seg_sites[[1]], 'locus') = rep(0, each = 4)
 
   phased <- unphase_segsites(seg_sites, 2, 1)
   expect_that(phased, is_a('list'))
   expect_equal(dim(phased[[1]]), c(2, 4))
   expect_equal(phased[[1]][2, ], c(1, 0, 0, 1))
   expect_equal(phased[[1]][1, c(1,3)], c(1, 0))
+  expect_equal(attr(phased[[1]], 'positions'),
+               attr(seg_sites[[1]], 'positions'))
+  expect_equal(attr(phased[[1]], 'locus'),
+               attr(seg_sites[[1]], 'locus'))
 
   phased <- unphase_segsites(seg_sites, 2, 2)
   expect_that(phased, is_a('list'))
@@ -22,6 +28,10 @@ test_that("unphasing works", {
   expect_equal(phased[[1]][2, c(1,3)], c(1, 0))
   expect_equal(phased[[1]][3, ], c(1, 0, 0, 1))
   expect_equal(phased[[1]][4, ], c(1, 0, 0, 1))
+  expect_equal(attr(phased[[1]], 'positions'),
+               attr(seg_sites[[1]], 'positions'))
+  expect_equal(attr(phased[[1]], 'locus'),
+               attr(seg_sites[[1]], 'locus'))
 
   seg_sites[[2]] <- seg_sites[[1]]
   phased <- unphase_segsites(seg_sites, 2, 1)
