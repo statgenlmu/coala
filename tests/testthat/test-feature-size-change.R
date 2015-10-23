2792e55ef7f5296eeb32f90a27bf14874cb8b58f
Paul Staab <develop@paulstaab.de>
2015-10-23 11:18:01 +0200

    fix simulation of sizes changes with 1pop in msms
    fixes #105

--- tests/testthat/test-feature-size-change.R
+++ tests/testthat/test-feature-size-change.R
@@ -1,10 +1,27 @@
 context("Feature Size Change")
 
 test_that("generating scrm cmd for size changes works", {
+  get_cmd <- get_simulator("scrm")$get_cmd
   model <- coal_model(15, 1) + feat_size_change(par_range("alpha", 0, 1), 1)
-  expect_equal(get_simulator("scrm")$get_cmd(model), "scrm 15 1 -en 0 1 alpha ")
-  model <- coal_model(15, 1) + feat_size_change(5, 1)
-  expect_equal(get_simulator("scrm")$get_cmd(model), "scrm 15 1 -en 0 1 5 ")
+  expect_equal(get_cmd(model), "scrm 15 1 -N alpha ")
+
+  model <- coal_model(15, 1) + feat_size_change(5, "all")
+  expect_equal(get_cmd(model), "scrm 15 1 -N 5 ")
+
+  model <- coal_model(15, 1) + feat_size_change(5, "all", 1)
+  expect_equal(get_cmd(model), "scrm 15 1 -eN 1 5 ")
+
+  model <- coal_model(14:15, 1) + feat_size_change(5, "all")
+  expect_equal(get_cmd(model), "scrm 29 1 -I 2 14 15 -N 5 ")
+
+  model <- coal_model(14:15, 1) + feat_size_change(5, "all", 1)
+  expect_equal(get_cmd(model), "scrm 29 1 -I 2 14 15 -eN 1 5 ")
+
+  model <- coal_model(14:15, 1) + feat_size_change(5, 2)
+  expect_equal(get_cmd(model), "scrm 29 1 -I 2 14 15 -n 2 5 ")
+
+  model <- coal_model(14:15, 1) + feat_size_change(5, 2, 1)
+  expect_equal(get_cmd(model), "scrm 29 1 -I 2 14 15 -en 1 2 5 ")
 })
 
 
--- tests/testthat/test-simulator-msms.R
+++ tests/testthat/test-simulator-msms.R
@@ -125,11 +125,37 @@ test_that("msms can simulate locus trios", {
 })
 
 
-test_that("ms can added manually", {
+test_that("msms can be added manually", {
   if (!has_msms()) skip("msms not installed")
   msms_jar <- get_simulator("msms")$get_info()["jar"]
   java <- get_simulator("msms")$get_info()["java"]
   activate_msms(msms_jar, java, 199)
   expect_equal(get_simulator("msms")$get_priority(), 199)
   expect_error(use_msms(tempfile("not-existant"), tempfile("not-existant")))
+})
+
+
+test_that("msms supports size changes in one pop models", {
+  if (!has_msms()) skip("msms not installed")
+
+  model <- coal_model(40, 1) +
+    feat_mutation(1) +
+    feat_size_change(0.1, population = 1, time = 0.1) +
+    sumstat_sfs()
+
+  stat <- get_simulator("msms")$simulate(model)
+  expect_that(stat$sfs, is_a("numeric"))
+})
+
+
+test_that("msms supports growth in one pop models", {
+  if (!has_msms()) skip("msms not installed")
+
+  model <- coal_model(40, 1) +
+    feat_mutation(1) +
+    feat_growth(0.1, population = 1, time = 0.1) +
+    sumstat_sfs()
+
+  stat <- get_simulator("msms")$simulate(model)
+  expect_that(stat$sfs, is_a("numeric"))
 })
