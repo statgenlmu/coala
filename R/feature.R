8d31e4ccc108f35feb684ac9bc6d527f9eae6a97
Paul Staab <develop@paulstaab.de>
2016-12-27 12:03:27 +0000

    add infrastructure for storing locus groups

--- R/feature.R
+++ R/feature.R
@@ -6,11 +6,16 @@ feature_class <- R6Class("feature", inherit = model_part,
     population = NULL,
     time = NULL,
     rate = NA,
+    locus_group = NULL,
     set_population = function(population, expected_length = 1) {
       assert_that(identical(population, "all") || is.numeric(population))
       assert_that(identical(population, "all") ||
                     length(population) == expected_length)
       private$population <- population
+    },
+    set_locus_group = function(locus_group) {
+      assert_that(identical(locus_group, "all") || is.numeric(locus_group))
+      private$locus_group <- locus_group
     },
     add_parameter = function(parameter, required = TRUE, add_par = TRUE) {
       expr <- NA
@@ -31,14 +36,16 @@ feature_class <- R6Class("feature", inherit = model_part,
     }
   ),
   public = list(
-    initialize = function(rate, population, time) {
+    initialize = function(rate, population, time, locus_group) {
       if (!missing(rate)) private$rate <- private$add_parameter(rate)
       if (!missing(time)) private$time <- private$add_parameter(time)
       if (!missing(population)) private$set_population(population)
+      if (!missing(locus_group)) private$set_locus_group(locus_group)
     },
     get_parameters = function() private$parameter,
     get_population = function() private$population,
     get_time = function() private$time,
+    get_locus_group = function() private$locus_group,
     reset_parameters = function() private$parameter <- list(),
     print = function() {
       cat("Feature of type", private$feature_table$type[1], "\n")
